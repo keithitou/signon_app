5370f0788a3bf513f995ad8ead0c296d45e7a8c2
Kei Itou <k.itou@nps.ne.jp>
2014-02-28 20:00:30 +0900

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,33 +2,27 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	subject { page }
+
   describe "Home page" do
+  	before { visit root_path }
     
-    it "should have the content 'SignOn App'" do
-    	visit '/static_pages/home'
-    	expect(page).to have_content('SignOn App')
-    end
+    it { should have_content('SignOn App') }
 
-    it "should have the title 'Home'" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title("Ruby on Rails SignOn App")
-    end
+    it { should have_title(full_title('')) }
 
-    it "should not have a custom page title" do
-    	visit '/static_pages/home'
-    	expect(page).not_to have_title('| Home')
-    end
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
 
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_content('Help')
   	end
 
   	it "should have the title 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_title("Ruby on Rails SignOn App | Help")
   	end
   end
@@ -36,12 +30,12 @@ describe "StaticPages" do
   describe "About page" do
 
   	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_content('About Us')
   	end
 
   	it "should have the title 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_title("Ruby on Rails SignOn App | About Us")
   	end
   end
